49f402b387c367d29c16f6b2bcdb96b794bfad48
EvanLDouglass <EvanLDouglass@gmail.com>
2019-05-25 22:03:23 -0700

    Add linear search and sentinel search

---
+++ algs/search/linear/linear.go
@@ -0,0 +1,41 @@
+// Package linear contains linear search algorithms. These are academic exercises.
+package linear
+
+// Search searches a slice of ints for the given value.
+// It returns the index of the first occurrence of value if found, or -1 if not found.
+func Search(arr []int, value int) int {
+	for i, v := range arr {
+		if v == value {
+			return i
+		}
+	}
+	return -1
+}
+
+// SentinelSearch searches a slice of ints for the given value using a
+// sentinel value strategy.
+// It returns the index of the first occurrence of value if found, or -1 if not found.
+func SentinelSearch(arr []int, value int) int {
+	length := len(arr)
+	if length == 0 {
+		return -1
+	}
+
+	// extract and replace last element
+	last := arr[length-1]
+	arr[length-1] = value
+	// search for value
+	i := 0
+	for arr[i] != value {
+		i++
+	}
+	// restore last element
+	arr[length-1] = last
+
+	if (i < length-1) || (arr[length-1] == value) {
+		// found
+		return i
+	}
+	// not found
+	return -1
+}
---
+++ algs/search/linear/linear_test.go
@@ -0,0 +1,73 @@
+package linear
+
+import "testing"
+
+func TestLinear(t *testing.T) {
+	cases := []struct {
+		inArr    []int
+		inVal    int
+		expected int
+	}{
+		// tests
+		{
+			[]int{}, 3, -1,
+		},
+		{
+			[]int{1}, 1, 0,
+		},
+		{
+			[]int{1}, 2, -1,
+		},
+		{
+			[]int{1, 2, 3, 4, 5}, 3, 2,
+		},
+		{
+			[]int{4, 3, 7, 1, 5, 0, 23, 90, 1000, 2, 5}, 5, 4,
+		},
+	}
+	for _, c := range cases {
+		// test Search
+		got := Search(c.inArr, c.inVal)
+		if got != c.expected {
+			t.Errorf("Search(%v, %v) == %v, expected %v", c.inArr, c.inVal, got, c.expected)
+		}
+
+		// test SentinelSearch
+		got = SentinelSearch(c.inArr, c.inVal)
+		if got != c.expected {
+			t.Errorf("SentinelSearch(%v, %v) == %v, expected %v", c.inArr, c.inVal, got, c.expected)
+		}
+	}
+}
+
+func BenchmarkSearch(b *testing.B) {
+	arr := make([]int, 10000)
+	val := 1 // will not be found
+	for i := 0; i < b.N; i++ {
+		Search(arr, val)
+	}
+}
+
+func BenchmarkSentinelSearch(b *testing.B) {
+	arr := make([]int, 10000)
+	val := 1 // will not be found
+	for i := 0; i < b.N; i++ {
+		SentinelSearch(arr, val)
+	}
+}
+
+func BenchmarkSearchLarge(b *testing.B) {
+	arr := make([]int, 1000000)
+	val := 1 // will not be found
+	for i := 0; i < b.N; i++ {
+		Search(arr, val)
+	}
+}
+
+func BenchmarkSentinelSearchLarge(b *testing.B) {
+	arr := make([]int, 1000000)
+	val := 1 // will not be found
+	for i := 0; i < b.N; i++ {
+		SentinelSearch(arr, val)
+	}
+}
